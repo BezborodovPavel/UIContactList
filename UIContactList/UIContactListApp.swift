1be114c2f52c4938ad6e60221f9772f1d6d5b861
BezborodovPavel <bezborodovp@gmail.com>
2022-07-21 12:31:56 +0500

    Initial Commit

---
+++ UIContactList/UIContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIContactListApp.swift
+//  UIContactList
+//
+//  Created by Павел on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
